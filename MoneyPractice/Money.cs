09850025adfff2e8a6146392857d91dd321345c0
Nurkaiyr <yedige21@gmail.com>
2017-12-25 01:05:09 +0600

    firstcommit

---
+++ MoneyPractice/Money.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyPractice
+{
+    public class Money
+    {
+        private int denomination;// Номинал 
+        private int count;//Количество 
+        private int totalMoney;
+
+        public Money(int denomination, int count)
+        {
+            this.denomination = denomination;
+            this.count = count;
+            this.totalMoney = denomination * count;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Номинал: {0}, Количество: {1}",denomination, count);
+        }
+
+        public bool IsEnough(int productPrice)
+        {
+            if (totalMoney > productPrice)
+            {
+                Console.WriteLine("У вас достаточно денег!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("У вас не достаточно денег!");
+                return false;
+            }
+        }
+
+        public int GetNumberOfPieces(int productPrice) 
+        {
+            if (productPrice != 0)
+            {
+                return totalMoney / productPrice;
+            }
+            else
+            {
+                Console.WriteLine("Не правильная цена продукта!");
+                return 0;
+            }
+        }
+
+        public int Denomination
+        {
+            get { return this.denomination; }
+            set { this.denomination = value; }
+        }
+
+        public int Count
+        {
+            get { return this.count; }
+            set { this.count = value; }
+        }
+
+        public int TotalMoney
+        {
+            get { return this.totalMoney; }
+        }
+    }
+}
